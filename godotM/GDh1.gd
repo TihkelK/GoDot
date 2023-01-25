b897edb2062ac50f2bd4a195b06b93cdc772edc8
TihkelK <116258789+TihkelK@users.noreply.github.com>
2023-01-25 09:58:28 +0200

    Add files via upload

---
+++ godotM/GDh1.gd
@@ -0,0 +1,15 @@
+#M.Kuusemae
+#18.01.23
+#h1
+
+extends Node
+
+func _ready():
+	var playerName = "Godot robot"
+	var playerHealth = 10
+	print("Nimi: ", playerName)
+	print("Elud: ", playerHealth)
+	print()
+	print(len(playerName))
+	print(playerHealth + 2)
+	print(randi() % 20)
---
+++ godotM/Harjutused/GDh2.gd
@@ -0,0 +1,14 @@
+#M.Kuusemae
+#25.01.23
+#h2
+
+extends Node
+
+func _ready():
+	var tooted = {
+			"toode1":{"maksab":50},
+			"toode2":{"maksab":99},
+			"toode3":{"maksab":150},
+}
+func _input(event):
+	Input()
